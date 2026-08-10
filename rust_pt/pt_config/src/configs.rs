f2df0aa5e8641faf8a9bc1e34f7a22833fe77b35
pryty26 <pryty017@proton.me>
2026-08-10 20:39:53 +0300

    feat: add ServerKey and ClientKey
    1.  add ServerKey and ClientKey
    2. improve docs
    3. replace TOR_PT_MANAGED_TRANSPORT_VER: String with TOR_PT_MANAGED_TRANSPORT_VER: Vec<String>

--- rust_pt/pt_config/src/configs.rs
+++ rust_pt/pt_config/src/configs.rs
@@ -3,16 +3,21 @@ use crate::MODE;
 use anyhow::Result;
 use pt_err::ConfigError;
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 use std::convert::TryFrom;
-use std::net::{Ipv4Addr, Ipv6Addr};
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 use std::path::PathBuf;
+use url::Url;
+/// PTs SHOULD ignore PT names that it does not recognize.
+/// So using String is acceptable, since we won't validate it.
+type PtTransportName = String;
 /// RawCommonKey is used to validate that the config is valid
 /// #[allow(unused)] is fine in this situation,
 /// since this is only to validate the config
 #[derive(Debug, Deserialize)]
 #[allow(unused)]
 pub(crate) struct RawCommonKey {
-    TOR_PT_MANAGED_TRANSPORT_VER: String,
+    TOR_PT_MANAGED_TRANSPORT_VER: Vec<String>,
     TOR_PT_STATE_LOCATION: PathBuf,
     TOR_PT_EXIT_ON_STDIN_CLOSE: i8,
     TOR_PT_OUTBOUND_BIND_ADDRESS_V4: Option<Ipv4Addr>,
@@ -58,9 +63,8 @@ impl TryFrom<RawCommonKey> for CommonKey {
                     message: format!("{}", e),
                 })?,
         };
-        let versions = separate_with(raw.TOR_PT_MANAGED_TRANSPORT_VER, ",");
         Ok(CommonKey {
-            TOR_PT_MANAGED_TRANSPORT_VER: versions,
+            TOR_PT_MANAGED_TRANSPORT_VER: raw.TOR_PT_MANAGED_TRANSPORT_VER,
             TOR_PT_STATE_LOCATION: raw.TOR_PT_STATE_LOCATION,
             TOR_PT_EXIT_ON_STDIN_CLOSE: raw.TOR_PT_EXIT_ON_STDIN_CLOSE,
             TOR_PT_OUTBOUND_BIND_ADDRESS_V4: TOR_PT_OUTBOUND_BIND_ADDRESS_V4,
@@ -76,12 +80,18 @@ impl TryFrom<RawCommonKey> for CommonKey {
 pub struct CommonKey {
     /// Comma-separated list of Pluggable Transport specification versions
     /// supported by the parent process.
+    /// Example:
+    /// "TOR_PT_MANAGED_TRANSPORT_VER": ["1.0", "2.0", "3.0"]
     TOR_PT_MANAGED_TRANSPORT_VER: Vec<String>,
 
     /// Directory where the Pluggable Transport may persist state.
+    /// Example:
+    /// "TOR_PT_STATE_LOCATION": "/var/lib/tor/state",
     TOR_PT_STATE_LOCATION: PathBuf,
 
     /// Whether the PT should gracefully exit when stdin is closed.
+    /// Example:
+    /// "TOR_PT_STATE_LOCATION": 1
     TOR_PT_EXIT_ON_STDIN_CLOSE: i8,
 
     /// Optional IPv4 source address for outbound connections.
@@ -97,151 +107,55 @@ pub struct CommonKey {
 
 /// Settings which is needed at client side
 #[derive(Debug, Serialize, Deserialize)]
-// TODO: #[serde(try_from = "?")]
 pub struct ClientKey {
-    TOR_PT_CLIENT_TRANSPORTS: Vec<String>,
+    TOR_PT_CLIENT_TRANSPORTS: Vec<PtTransportName>,
+    TOR_PT_PROXY: Option<Url>,
 }
 
-/*
-use std::ffi::OsString;
-use std::fmt;
-
-pub(crate) const TRANSPORT_NAME: &str = "proteus";
-pub(crate) const SUPPORTED_PT_VERSION: &str = "1";
-pub(crate) const SUPPORTED_SOCKS_VERSION: &str = "socks5";
-
-#[derive(Debug)]
-#[allow(non_camel_case_types)]
-pub(crate) enum CommonKey {
-    TOR_PT_MANAGED_TRANSPORT_VER,
-    TOR_PT_STATE_LOCATION,
-    TOR_PT_EXIT_ON_STDIN_CLOSE,
-    TOR_PT_OUTBOUND_BIND_ADDRESS_V4,
-    TOR_PT_OUTBOUND_BIND_ADDRESS_V6,
+/// Specifies per-PT protocol configuration directives,
+///  as a semicolon-separated list of <key>:<value> pairs,
+///  where <key> is a PT name and <value> is a k=v string value
+/// with options that are to be passed to the transport.
+/// Example:
+/// TOR_PT_SERVER_TRANSPORT_OPTIONS=scramblesuit:key=banana;automata:rule=110;automata:depth=3
+#[derive(Debug, Serialize, Deserialize)]
+pub struct TransportOptions {
+    name: PtTransportName,
+    settings: HashMap<String, String>,
 }
 
-impl CommonKey {
-    pub(crate) fn as_str(&self) -> &'static str {
-        match self {
-            CommonKey::TOR_PT_MANAGED_TRANSPORT_VER => "TOR_PT_MANAGED_TRANSPORT_VER",
-            CommonKey::TOR_PT_STATE_LOCATION => "TOR_PT_STATE_LOCATION",
-            CommonKey::TOR_PT_EXIT_ON_STDIN_CLOSE => "TOR_PT_EXIT_ON_STDIN_CLOSE",
-            CommonKey::TOR_PT_OUTBOUND_BIND_ADDRESS_V4 => "TOR_PT_OUTBOUND_BIND_ADDRESS_V4",
-            CommonKey::TOR_PT_OUTBOUND_BIND_ADDRESS_V6 => "TOR_PT_OUTBOUND_BIND_ADDRESS_V6",
-        }
-    }
+/// Settings which is needed at server side
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ServerKey {
+    /// Specifies the PT protocols the server proxy should initialize, as a comma separated list of PT names.
+    /// PTs SHOULD ignore PT names that it does not recognize.
+    /// Example:
+    /// TOR_PT_SERVER_TRANSPORTS=obfs3,scramblesuit
+    TOR_PT_SERVER_TRANSPORTS: Vec<PtTransportName>,
+    /// Specifies per-PT protocol configuration directives,
+    ///  as a semicolon-separated list of <key>:<value> pairs,
+    ///  where <key> is a PT name and <value> is a k=v string value
+    /// with options that are to be passed to the transport.
+    /// Example:
+    /// TOR_PT_SERVER_TRANSPORT_OPTIONS=scramblesuit:key=banana;automata:rule=110;automata:depth=3
+    TOR_PT_SERVER_TRANSPORT_OPTIONS: Option<TransportOptions>,
+    /// A comma separated list of <key>-<value> pairs,
+    /// where <key> is a PT name and <value> is the <address>:<port>
+    /// on which it should listen for incoming client connections.
+    ///Example:
+    /// TOR_PT_SERVER_BINDADDR=obfs3-198.51.100.1:1984,scramblesuit-127.0.0.1:4891
+    TOR_PT_SERVER_BINDADDR: Vec<IpAddr>,
+    /// Specifies the destination that
+    /// the PT reverse proxy should forward traffic to after transforming it as appropriate,
+    ///  as an <address>:<port>.
+
+    /// Connections to the destination specified via “TOR_PT_ORPORT” MUST only contain application payload.
+    ///  If the parent process requires the actual source IP address of client connections (or other metadata),
+    ///  it should set “TOR_PT_EXTENDED_SERVER_PORT” instead.
+    TOR_PT_ORPORT: IpAddr,
+    TOR_PT_EXTENDED_SERVER_PORT: IpAddr,
+    TOR_PT_AUTH_COOKIE_FILE: PathBuf,
 }
 
-impl fmt::Display for CommonKey {
-    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(fmt, "{:?}", self)
-    }
-}
-
-impl From<CommonKey> for &str {
-    fn from(value: CommonKey) -> Self {
-        value.as_str()
-    }
-}
-
-impl From<CommonKey> for String {
-    fn from(value: CommonKey) -> Self {
-        value.to_string()
-    }
-}
-
-impl From<CommonKey> for OsString {
-    fn from(value: CommonKey) -> Self {
-        OsString::from(String::from(value))
-    }
-}
-
-#[derive(Debug)]
-#[allow(non_camel_case_types)]
-pub(crate) enum ClientKey {
-    TOR_PT_CLIENT_TRANSPORTS,
-    TOR_PT_PROXY,
-}
-
-impl ClientKey {
-    pub(crate) fn as_str(&self) -> &'static str {
-        match self {
-            ClientKey::TOR_PT_CLIENT_TRANSPORTS => "TOR_PT_CLIENT_TRANSPORTS",
-            ClientKey::TOR_PT_PROXY => "TOR_PT_PROXY",
-        }
-    }
-}
-
-impl fmt::Display for ClientKey {
-    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(fmt, "{:?}", self)
-    }
-}
-
-impl From<ClientKey> for &str {
-    fn from(value: ClientKey) -> Self {
-        value.as_str()
-    }
-}
-
-impl From<ClientKey> for String {
-    fn from(value: ClientKey) -> Self {
-        value.to_string()
-    }
-}
-
-impl From<ClientKey> for OsString {
-    fn from(value: ClientKey) -> Self {
-        OsString::from(String::from(value))
-    }
-}
-
-#[derive(Debug)]
-#[allow(non_camel_case_types)]
-pub(crate) enum ServerKey {
-    TOR_PT_SERVER_TRANSPORTS,
-    TOR_PT_SERVER_TRANSPORT_OPTIONS,
-    TOR_PT_SERVER_BINDADDR,
-    TOR_PT_ORPORT,
-    TOR_PT_EXTENDED_SERVER_PORT,
-    TOR_PT_AUTH_COOKIE_FILE,
-}
-
-impl ServerKey {
-    pub(crate) fn as_str(&self) -> &'static str {
-        match self {
-            ServerKey::TOR_PT_SERVER_TRANSPORTS => "TOR_PT_SERVER_TRANSPORTS",
-            ServerKey::TOR_PT_SERVER_TRANSPORT_OPTIONS => "TOR_PT_SERVER_TRANSPORT_OPTIONS",
-            ServerKey::TOR_PT_SERVER_BINDADDR => "TOR_PT_SERVER_BINDADDR",
-            ServerKey::TOR_PT_ORPORT => "TOR_PT_ORPORT",
-            ServerKey::TOR_PT_EXTENDED_SERVER_PORT => "TOR_PT_EXTENDED_SERVER_PORT",
-            ServerKey::TOR_PT_AUTH_COOKIE_FILE => "TOR_PT_AUTH_COOKIE_FILE",
-        }
-    }
-}
-
-impl fmt::Display for ServerKey {
-    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(fmt, "{:?}", self)
-    }
-}
-
-impl From<ServerKey> for &str {
-    fn from(value: ServerKey) -> Self {
-        value.as_str()
-    }
-}
-
-impl From<ServerKey> for String {
-    fn from(value: ServerKey) -> Self {
-        value.to_string()
-    }
-}
-
-impl From<ServerKey> for OsString {
-    fn from(value: ServerKey) -> Self {
-        OsString::from(String::from(value))
-    }
-}
-
+/*let config: TorConfig = envy::from_env()?;
 */
